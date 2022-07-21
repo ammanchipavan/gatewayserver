dade00a85f5df83e7cc3cc0fae4b0f7aab4f8e98
Pavan Kumar Ammanchi <ammanchipavan@users.noreply.github.com>
2022-07-21 23:00:45 +0530

    Adding OAuth2.0 to Gateway

---
+++ src/main/java/com/pavan/gatewayserver/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.pavan.gatewayserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author PAVAN
+ *
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http.authorizeExchange(exchanges -> exchanges.pathMatchers("/pavanbank/accounts/**").authenticated()
+                        .pathMatchers("/pavanbank/cards/**").authenticated()
+                        .pathMatchers("/pavanbank/loans/**").permitAll())
+                .oauth2ResourceServer().jwt();
+        http.csrf().disable();
+        return http.build();
+    }
+}

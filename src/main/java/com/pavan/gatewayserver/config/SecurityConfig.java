08be7554f1341455840c31e95fd99b0f6e4a370e
Pavan Kumar Ammanchi <ammanchipavan@users.noreply.github.com>
2022-07-22 15:11:54 +0530

    KeyCloak authentication and authorization

--- src/main/java/com/pavan/gatewayserver/config/SecurityConfig.java
+++ src/main/java/com/pavan/gatewayserver/config/SecurityConfig.java
@@ -5,9 +5,16 @@ package com.pavan.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import reactor.core.publisher.Mono;
 
 /**
  * @author PAVAN
@@ -19,11 +26,20 @@ public class SecurityConfig {
 	
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchanges -> exchanges.pathMatchers("/pavanbank/accounts/**").authenticated()
+        http.authorizeExchange(exchanges -> exchanges.pathMatchers("/pavanbank/accounts/**").hasRole("MANAGER")
                         .pathMatchers("/pavanbank/cards/**").authenticated()
                         .pathMatchers("/pavanbank/loans/**").permitAll())
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(grantedAuthoritiesExtractor());
         http.csrf().disable();
         return http.build();
     }
+    
+    Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeyCloakJwtRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
+    }
+
 }
